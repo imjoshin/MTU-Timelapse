cbc9e48d8f01981e6e76604d169f13a0ceb541fa
j0sh77 <jocjohns@mtu.edu>
2015-05-21 09:55:52 -0400

    Initial commit.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){  
+/*
+	$.ajax({
+		type: 'POST',
+		url: 'php/ajax.php',
+		async: false,
+		success: function(output){
+			console.log(output);
+		},
+		error: function(output){
+			alert("An error occured.");
+		}
+	});
+*/
+
+});
